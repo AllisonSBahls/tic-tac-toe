56d56275ceb71d0b70f67cb05a136812251e576d
AllisonSBahls <allison_sousa_bahls@hotmail.com>
2019-07-07 20:12:06 -0300

    Jogo da Velha

--- dist/script.js
+++ dist/script.js
@@ -1,24 +1,176 @@
-window.onload = () =>{
+window.onload = () => {
     new JogoVelha();
 };
 
-class JogoVelha{
-    constructor(){
+class JogoVelha {
+    constructor() {
         this.iniciaElementos();
+        this.iniciaEstado();
 
     }
 
-    iniciaElementos(){
+    iniciaEstado() {
+        this.turno = true;
+        this.jogadas = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+        this.fim = false;
+        this.vitoria = [448, 56, 7, 292, 146, 73, 273, 84];
+    }
+
+    iniciaElementos() {
+
+
+        this.salvar = document.querySelector("#salvar");
+        this.salvar.addEventListener('click', this.enviarServidor.bind(this));
+
+
+        this.limparLocal = document.querySelector('#limpar');
+        this.limparLocal.addEventListener('click', this.limpaLocal.bind(this));
+
+        this.jogadorX = document.querySelector("#jogador-x");
+        this.jogadorO = document.querySelector("#jogador-o");
+
+
+        this.salvarLocal = document.querySelector("#salva-local");
+        this.salvarLocal.addEventListener('click', this.salvaLocal.bind(this));
+
+        this.carregarLocal = document.querySelector("#carrega-local");
+        this.carregarLocal.addEventListener('click', this.carregaLocal.bind(this));
+
+
         this.velha = document.querySelector('#velha');
-        this.velha.addEventListener('click', this.realizaJogada);
+        this.velha.addEventListener('click', (event) => {
+            this.realizaJogada(event);
+            this.render();
+        });
     }
 
-    realizaJogada(event){
-        event.target.innerHTML = this.turno ? 'X' : 'O';
-        this.turno  = !this.turno;
+    realizaJogada(event) {
+        const id = event.target.dataset.id;
+
+        if (this.fim) {
+            this.modal("Partida terminada");
+            return;
+        }
+
+        if (!event.target.dataset.id) {
+            this.modal("voce precisa clicar em uma casa")
+            return;
+        }
+
+        if (this.jogadas[id] != 0) {
+            this.modal("Posição já foi escolhida")
+            return;
+        }
+
+        this.jogadas[id] = this.turno ? 'X' : 'O';
+        this.turno = !this.turno;
     }
 
-    render(){
-        
+    render() {
+        const resultado = this.verificaVitoria();
+
+        if (resultado == 'X' || resultado == 'O') {
+            this.fim = true;
+            this.salvar.style.display = "block";
+
+            this.modal(`O jogador ${resultado} venceu!`);
+        }else{
+            this.salvar.style.display = "none";
+
+        }
+
+        const velhaElemento = document.querySelectorAll('[data-id]');
+
+        for (let i = 0; i < 9; i++) {
+            velhaElemento[i].innerHTML = this.jogadas[i] == 0 ? "" : this.jogadas[i];
+        }
+    }
+
+    salvaLocal() {
+
+        const dados = {
+            jogadorX: this.jogadorX.value,
+            jogadorO: this.jogadorO.value,
+            jogadas: this.jogadas,
+        }
+
+        localStorage.setItem('jogo', JSON.stringify(dados));
+    }
+
+    carregaLocal() {
+        const dados = JSON.parse(localStorage.getItem('jogo'));
+        this.jogadorO.value = dados.jogadorO;
+        this.jogadorX.value = dados.jogadorX;
+        this.jogadas = dados.jogadas;
+
+        this.render();
+    }
+
+    limpaLocal() {
+        localStorage.removeItem('jogo');
+        this.jogadorO.value = '';
+        this.jogadorX.value = '';
+        this.iniciaEstado();
+
+        this.render();
+    }
+
+    verificaVitoria() {
+        const valorX = parseInt(this.jogadas.map(value => value == 'X' ? 1 : 0).join(''), 2);
+
+        const valorO = parseInt(this.jogadas.map(value => value == 'O' ? 1 : 0).join(''), 2);
+
+        for (const element of this.vitoria) {
+            if ((element & valorX) == element) {
+                return 'X'
+            }
+            if ((element & valorO) == element) {
+                return 'O'
+            }
+        }
+
+        return "";
+    }
+
+    modal(texto) {
+        const modais = document.querySelector("#modais");
+        const modal = document.createElement("div");
+        modal.innerHTML = texto;
+        modal.classList.add("modalClass");
+        modais.appendChild(modal);
+
+
+        setTimeout(() => {
+            modal.classList.add("remover");
+
+            setTimeout(() => {
+                modais.removeChild(modal);
+
+            }, 1000);
+
+        }, 2000);
+    }
+
+    enviarServidor() {
+        const jogadorX = this.jogadorX.value;
+        const jogadorO = this.jogadorO.value;
+
+        domtoimage.toPng(this.velha, {width: '400', height: '400'})
+            .then((dataUrl) => {
+                return axios.post('/save', {
+                    jogadorX, jogadorO, jogadas : JSON.stringify(this.jogadas),
+                    img: dataUrl
+                })
+
+
+            }).then((response) => {
+                this.modal("Jogo enviados com sucesso");
+            })
+
+            .catch((error) => {
+                this.modal('oops, something went wrong!', error);
+
+            });
+
     }
 }
